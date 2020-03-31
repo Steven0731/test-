c81639f4905ffd2c5675871c993ca5d2b3075ca9
Steven0731 <JYChen0731@outlook.com>
2020-03-31 21:41:22 +0800

    第二次修改

---
+++ st.cpp
@@ -0,0 +1,331 @@
+#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}#include<iostream>
+using namespace std;
+int mian()
+{
+	int a,b;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		b+=i;}
+	cout<<b<<endl;
+	return 0;
+}
